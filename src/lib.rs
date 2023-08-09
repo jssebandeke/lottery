f4c15a3dbec94411434e93f3c5f1edb9ddefd854
jjssebandeke <jjssebandeke@outlook.com>
2023-08-09 21:30:57 +0100

    feat: implement lottery fuctionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{near_bindgen, AccountId, env, require, Promise, ONE_NEAR};
-use near_sdk::store::{Vector,LookupMap};
+use near_sdk::{near_bindgen, AccountId, env, require, Promise, ONE_NEAR, log};
+use near_sdk::store::{UnorderedSet,LookupMap};
 use near_sdk::json_types::U128;
 
 #[near_bindgen]
@@ -9,15 +9,16 @@ pub struct Lottery {
 
     pub lottery_id: u32,
     pub owner: AccountId,
-    pub players: Vector<AccountId>,
+    pub players: UnorderedSet<AccountId>,
     pub winners: LookupMap<u32, AccountId>
     
 }
 
 impl Default for Lottery {
     fn default() -> Self {
-        Self { owner: env::signer_account_id() ,
-               players: Vector::new(b'i'), 
+        Self { 
+               owner: env::signer_account_id() ,
+               players: UnorderedSet::new(b'i'), 
                lottery_id: Default::default(),
                winners: LookupMap::new(b'w')
         }
@@ -31,13 +32,16 @@ impl Lottery {
 
     #[payable]
     pub fn enter(&mut self){
-        require!(env::attached_deposit() > ONE_NEAR, "Not Engough Near Was Sent!");
+        require!(env::attached_deposit() == ONE_NEAR, "Not Engough Near Was Sent!");
 
-        self.players.push(env::signer_account_id())
+        self.players.insert(env::predecessor_account_id());
+
+
+        log!("{},Entered lottery", env::predecessor_account_id());
 
     }
 
-    pub fn get_balance() -> U128 {
+    pub fn get_balance(self) -> U128 {
         near_sdk::json_types::U128(env::account_balance())
     }
 
@@ -53,9 +57,20 @@ impl Lottery {
     fn select_winner(&mut self) -> Option<&AccountId> {
 
         let winner = self.get_random_number() %  self.players.len();
-        let player = self.players.get(winner).expect("Array should not be empty").to_owned();
+        let mut player = String::new();
+        
+        for (index, id) in self.players.into_iter().enumerate() {
+            
+            if index  == usize::try_from(winner).unwrap() {
+                player = id.to_string();
+                break
+            }
 
-        self.winners.insert(self.lottery_id, player);
+        }
+
+        self.winners.insert(self.lottery_id, AccountId::new_unchecked(player.clone()));
+
+        log!("{} won the lottery !",player);
         
         self.players.clear();
         let winner = self.winners.get(&self.lottery_id.to_owned());
@@ -66,28 +81,58 @@ impl Lottery {
         
     }
 
-
     pub fn claim(&mut self) -> Promise {
 
-        require!(env::signer_account_id() == self.owner.to_owned());
+        require!(env::predecessor_account_id() == self.owner.to_owned() || self.players.contains(&env::predecessor_account_id()));
 
         let winner = self.select_winner().unwrap();
+        
+        log!("Reset counter to zero");
 
         Promise::new(winner.to_owned()).transfer(env::account_balance())
 
     }
 
     
+    
 }
-
 
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
+    use near_sdk::{testing_env, VMContext};
+    use near_sdk::test_utils::VMContextBuilder;
+
+    fn init_context() -> VMContextBuilder {
+       VMContextBuilder::new()
+    }
 
     #[test]
-    fn it_works() {
-       
+    fn start_lottery() {
+
+       let contract = Lottery::default();
+
+       let ctx = init_context();
+       testing_env!(ctx.build());
+
+       assert_eq!(contract.owner.to_string(), "bob.near".to_string(), "Contract succesfully Initailzed");
+
     }
+    
+    #[test]
+    fn enter_lottery() {
+
+       let mut ctx = init_context().attached_deposit(ONE_NEAR).to_owned();
+
+       testing_env!(ctx.signer_account_id("test.near".parse().unwrap()).build());
+
+       let mut contract = Lottery::default();
+       contract.enter();
+
+       assert!(contract.players.contains(&AccountId::new_unchecked("test.near".to_owned())));
+  
+    }
+
 }
